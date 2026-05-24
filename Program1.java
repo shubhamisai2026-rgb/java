5830acd0f58cae18d20f6e827fc8feaaa0a216f4
shubhamisai <shubhamisai2026@gmail.com>
2026-05-25 00:44:56 +0530

    commited

---
+++ Program1.java
@@ -0,0 +1,8 @@
+public class Program1 {
+    public static void main(String[] args) {
+        System.out.println("All power within you. ");
+       System.out.println(" You can do anything and everything.   ");
+        System.out.println(" Believe in that; don't believe in your limitations. ");
+        System.out.println(" You will be unstoppable.    ");
+    }
+}
